e9caed3e47a478c17c944ea228e41678b9599cd2
Quintin J Goraseb <quintingoraseb@gmail.com>
2022-09-12 23:15:11 +0200

    Initial comit

---
+++ assets/js/main.js
@@ -0,0 +1,44 @@
+  // Clients carousel (uses the Owl Carousel library)
+  $(".clients-carousel").owlCarousel({
+    autoplay: true,
+    dots: true,
+    loop: true,
+    responsive: {
+      0: {
+        items: 2
+      },
+      768: {
+        items: 4
+      },
+      900: {
+        items: 6
+      }
+    }
+  });
+
+  // Events carousel (uses the Owl Carousel library)
+  $(".events-carousel").owlCarousel({
+    autoplay: true,
+    dots: true,
+    loop: true,
+    items: 1
+  });
+
+   // Initiate venobox lightbox
+   $(document).ready(function() {
+    $('.venobox').venobox();
+  });
+
+  	// Scrollax
+    $.Scrollax();
+
+  // Init AOS
+  function aos_init() {
+    AOS.init({
+      duration: 1000,
+      once: true
+    });
+  }
+  $(window).on('load', function() {
+    aos_init();
+  });
